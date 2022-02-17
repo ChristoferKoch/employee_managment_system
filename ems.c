cf3150cf90b0f975e477b4d1159df149648db18f
Christofer Koch <cbkoch13@gmail.com>
2022-02-16 20:23:10 -0800

    Add ems.c and basic structure

---
+++ ems.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct date {
+  int month, day, year;
+};
+  
+struct employee {
+  char lastname[15];
+  char firstname[15];
+  int employeeid;
+  struct date dob; //date of birth
+  struct date hire; //date hired
+};
+  
+int main()
+{
+  
+}
